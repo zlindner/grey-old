2cd0fe1a569398c4d0a1d34de000c8011cf30c6a
zlindner <zach.lindner@hotmail.com>
2018-05-23 11:49:08 -0400

    started implementing database

--- src/com/blackandwhite/grey/Grey.java
+++ src/com/blackandwhite/grey/Grey.java
@@ -7,10 +7,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class Grey extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        /*String query = "CREATE TABLE IF NOT EXISTS CUSTOMER (" +
+                "customer_id INT AUTO_INCREMENT PRIMARY KEY," +
+                "first_name VARCHAR(30) NOT NULL," +
+                "last_name VARCHAR(30) NOT NULL," +
+                "city VARCHAR(30)," +
+                "province VARCHAR(30)," +
+                "postal_code VARCHAR(6)," +
+                "address VARCHAR(30)," +
+                "email VARCHAR(30)," +
+                "work_phone VARCHAR(10)," +
+                "home_phone VARCHAR(10)," +
+                "cell_phone VARCHAR(10))";
+
+        Statement statement = connection.createStatement();
+        statement.execute(query);*/
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("grey.fxml"));
 
         Parent root = loader.load();
@@ -28,6 +47,16 @@ public class Grey extends Application {
     }
 
     public static void main(String[] args) {
+        Database.establishConnection();
+        Connection con = Database.getConnection();
+
+        if (con == null) {
+            System.out.println("Error establishing connection to database");
+            return;
+        }
+
+
+
         Application.launch(args);
     }
 }
--- src/com/blackandwhite/grey/customers/ControllerCustomers.java
+++ src/com/blackandwhite/grey/customers/ControllerCustomers.java
@@ -6,6 +6,8 @@ import javafx.css.PseudoClass;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.util.regex.Pattern;
+
 public class ControllerCustomers extends Controller {
 
     private static Modal add;
@@ -72,7 +74,63 @@ public class ControllerCustomers extends Controller {
         String cell = cellField.getText();
         String home = homeField.getText();
 
-        if (validateAdd(first, last, city, province, postal, address, email, work, cell, home)) {
+
+
+        if (!postal.isEmpty()) {
+            if (validatePostal(postal)) {
+                postal = postal.replaceAll("\\s", "");
+                postalField.pseudoClassStateChanged(error, false);
+
+                System.out.println(postal);
+            } else {
+                postalField.pseudoClassStateChanged(error, true);
+            }
+        }
+
+        if (!email.isEmpty()) {
+            if (validateEmail(email)) {
+                emailField.pseudoClassStateChanged(error, false);
+
+                System.out.println(email);
+            } else {
+                emailField.pseudoClassStateChanged(error, true);
+            }
+        }
+
+        if (!work.isEmpty()) {
+            if (validateNumber(work)) {
+                work = work.replaceAll("[^\\d.]", "");
+                workField.pseudoClassStateChanged(error, false);
+
+                System.out.println(work);
+            } else {
+                workField.pseudoClassStateChanged(error, true);
+            }
+        }
+
+        if (!cell.isEmpty()) {
+            if (validateNumber(cell)) {
+                cell = cell.replaceAll("[^\\d.]", "");
+                cellField.pseudoClassStateChanged(error, false);
+
+                System.out.println(cell);
+            } else {
+                cellField.pseudoClassStateChanged(error, true);
+            }
+        }
+
+        if (!home.isEmpty()) {
+            if (validateNumber(home)) {
+                home = home.replaceAll("[^\\d.]", "");
+                homeField.pseudoClassStateChanged(error, false);
+
+                System.out.println(home);
+            } else {
+                homeField.pseudoClassStateChanged(error, true);
+            }
+        }
+
+        /*if (validateAdd(first, last, city, province, postal, address, email, work, cell, home)) {
             Customer customer = new Customer.Builder()
                     .first(first)
                     .last(last)
@@ -86,8 +144,67 @@ public class ControllerCustomers extends Controller {
                     .home(home)
                     .build();
 
+            Connection con = Database.getConnection();
+            String query = "INSERT INTO CUSTOMER (" +
+                    "first_name," +
+                    "last_name," +
+                    "city," +
+                    "province," +
+                    "postal_code," +
+                    "address," +
+                    "email," +
+                    "work_phone," +
+                    "cell_phone," +
+                    "home_phone) " +
+                    "VALUES (" +
+                    "'" + first + "'," +
+                    "'" + last + "'," +
+                    "'" + city + "'," +
+                    "'" + province + "'," +
+                    "'" + postal + "'," +
+                    "'" + address + "'," +
+                    "'" + email + "'," +
+                    "'" + work + "'," +
+                    "'" + cell + "'," +
+                    "'" + home + "')";
+
+            Statement st = null;
+
+            try {
+                st = con.createStatement();
+                st.execute(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
             System.out.println(customer.toString());
             add.close();
+        }*/
+    }
+
+    private boolean validatePostal(String postal) {
+        if (Pattern.matches("^[0-9a-zA-Z ]*$", postal)) {
+            postal = postal.replaceAll("\\s", "");
+
+            return postal.length() == 6;
+        }
+
+        return false;
+    }
+
+    private boolean validateEmail(String email) {
+        Pattern regex = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+
+        return regex.matcher(email).matches();
+    }
+
+    private boolean validateNumber(String num) {
+        if (Pattern.matches("^[0-9- ]*$", num)) {
+            num = num.replaceAll("[^\\d.]", "");
+
+            return num.length() == 10;
+        } else {
+            return false;
         }
     }
 
