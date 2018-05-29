da5bcd1445a007c0379132f330d50fbbbef15d67
zlindner <zach.lindner@hotmail.com>
2018-05-29 16:27:00 -0400

    added app icons, should work on windows

--- src/com/blackandwhite/grey/Grey.java
+++ src/com/blackandwhite/grey/Grey.java
@@ -4,6 +4,7 @@ import com.blackandwhite.grey.login.ControllerLogin;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -24,6 +25,12 @@ public class Grey extends Application {
         stage.setScene(scene);
         stage.setResizable(false);
         stage.initStyle(StageStyle.UNDECORATED);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../../../assets/icon_16.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../../../assets/icon_32.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../../../assets/icon_64.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../../../assets/icon_128.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../../../assets/icon_256.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../../../assets/icon_512.png")));
 
         root.requestFocus();
 
