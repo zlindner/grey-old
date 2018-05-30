12e67cfaa74346eff139b152faeab0db4a50819a
zlindner <zach.lindner@hotmail.com>
2018-05-30 14:16:50 -0400

    added wip leak indicator on dashboard

--- src/com/blackandwhite/grey/dashboard/ControllerDashboard.java
+++ src/com/blackandwhite/grey/dashboard/ControllerDashboard.java
@@ -1,7 +1,10 @@
 package com.blackandwhite.grey.dashboard;
 
 import com.blackandwhite.grey.DataSource;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
 import org.apache.commons.dbcp2.BasicDataSource;
 
@@ -15,6 +18,9 @@ public class ControllerDashboard {
     @FXML
     private Label lblNewCustomers;
 
+    @FXML
+    private PieChart pcLeaks;
+
     public void initialize() {
         try {
             lblCustomers.setText(String.valueOf(getCustomers()));
@@ -23,6 +29,16 @@ public class ControllerDashboard {
         }
 
         lblNewCustomers.setText(getNewCustomers() + " New ↑");
+
+
+        int open = 13;
+        int closed = 61;
+
+        ObservableList<PieChart.Data> data = FXCollections.observableArrayList(
+                new PieChart.Data("Open", open),
+                new PieChart.Data("Closed", closed));
+
+        pcLeaks.setData(data);
     }
 
     private int getCustomers() throws SQLException {
@@ -43,6 +59,6 @@ public class ControllerDashboard {
 
     //TODO, new customers -> weekly
     private int getNewCustomers() {
-        return 1;
+        return 0;
     }
 }
